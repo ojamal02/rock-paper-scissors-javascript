7e6fabde32a60cf7f96f69534b75be000e6167d6
Omar Jamal <ojamal02@gmail.com>
2022-05-18 17:42:15 -0700

    Providing code commentary

--- js/index.js
+++ js/index.js
@@ -1,20 +1,25 @@
+// assigning variables using event lisener to trigger "run" function on click event for choice buttons
 var Rock = document.getElementById('Rock').addEventListener('click', run);
 var Paper = document.getElementById('Paper').addEventListener('click', run);
 var Scissors = document.getElementById('Scissors').addEventListener('click', run);
 
+// Establishing variables for the comparison function
 var computerChoice;
 var playerChoice;
 
+// establishing variables that will dynamically populate HTML based on game circumstances and outcome
 var player = document.getElementById('player');
 var computer = document.getElementById('computer');
 var outcome = document.getElementById('outcome');
 
+// run function establishes player and computer choices and then compares them
 function run() {
   	playerChoice = this.innerText;
   	computerGamble();
   	compare();
 }
 
+// function that randomly generates value between 0 and 1 and then conditionally assigns them to a choice
 function computerGamble() {
   	var dice = Math.random();
 	if (dice <= 0.33) {
@@ -25,7 +30,8 @@ function computerGamble() {
     	computerChoice = "Scissors";
     }
  }
- 
+
+// function that takes input of user and computer choices and then presents outcome based off conditional logic
  function compare() {
  	player.innerHTML = "You have chosen " + playerChoice;
  	computer.innerHTML = "Computer chooses " + computerChoice;
